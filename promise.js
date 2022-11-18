fe305dca334a2ce2022aa28b81983dee8fb6f431
Oleksii-Kramar <dp020991kos@gmail.com>
2022-11-18 23:22:16 +0100

    JS_promise_commit

---
+++ promise.js
@@ -0,0 +1,229 @@
+/*const promise=new Promise((resolve,reject)=>{
+        try {
+            setTimeout(() => {
+                resolve(10);
+            }, 1500);
+            
+        } catch (error) {
+            reject(error)
+        }finally{
+            console.log("abc")
+        }
+    })
+    const promise2=new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                resolve(10);
+            }, 1500);
+            setTimeout(() => {
+                reject(error)
+            }, 1000);
+    })
+    promise2.then(result=>console.log(result)).catch(err=>console.log(err)).finally(()=>{
+        console.log("end")
+    });
+                                       -> resolve, state: "fulfilled"
+    przez 500ms -> state: "pending"
+                                        -> reject, state: "rejected"
+    
+    async function getData(){
+        const response=await fetch("https://jsonplaceholder.typicode.com/users")
+        const data=await response.json()
+        return data;
+    }
+    getData().then((data)=>{
+        console.log("THEN",data)
+    })
+    
+
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(() => resolve("Job done!"), 1000);
+      });
+
+      promise.then(
+        result => alert(result), // wyświetli "done" po 1 sekundzie
+        error => alert(error) // nie uruchomi się
+      );
+
+      let promise = new Promise(function(resolve, reject) {
+        setTimeout(() => reject(new Error("Job failed!")), 1000);
+      });
+      
+      promise.then(
+        result => alert(result), // nie uruchomi się
+        error => alert(error) // wyświetli "Job failed!" 
+      );
+
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(() => reject(new Error("Job failed!")), 1000);
+      });
+      
+      promise.catch(alert);
+
+      function checkMail() {
+        return new Promise((resolve, reject) => {
+          if (Math.random() > 0.5) {
+            resolve('Mail wysłany');
+          } else {
+            reject(new Error('Nie udało się wysłać maila'));
+          }
+        });
+      }
+      
+      checkMail()
+        .then(console.log)
+        .catch(console.error)
+        .finally(() => {
+          console.log('Operacja zakończona');
+        });
+
+        function loadScript(src) {
+            return new Promise(function(resolve, reject) {
+              let script = document.createElement('script');
+              script.src = src;
+          
+              script.onload = () => resolve(script);
+              script.onerror = () => reject(new Error(`Script load error for ${src}`));
+          
+              document.head.append(script);
+            });
+          }
+          
+          
+          let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js");
+          
+          promise.then(
+            script => alert(`${script.src} is loaded!`),
+            error => alert(`Error: ${error.message}`)
+          );
+          
+          alert("Loading basic code (that does not require ajax library)");
+
+
+          function loadImageAsync(url) {
+            return new Promise(function(resolve, reject) {
+              let image = new Image();
+          
+              image.onload = function() {
+                resolve(image);
+              };
+          
+              image.onerror = function() {
+                reject(new Error('Could not load image at ' + url));
+              };
+          
+              image.src = url;
+            });
+          }
+          
+          loadImageAsync('one.png')
+            .then(function(image) {
+              console.log('Image loaded', image);
+            }, function(err) {
+              console.error('Error loading image', err);
+            });
+
+
+            new Promise(function(resolve, reject) {
+
+                setTimeout(() => resolve(1), 1000); 
+              
+              }).then(function(result) {
+              
+                alert(result); 
+                return result * 2;
+              
+              }).then(function(result) {
+              
+                alert(result);
+                return result * 5;
+              
+              }).then(function(result) {
+              
+                alert(result);
+                return result * 6;
+                
+                
+              
+              });
+
+
+              fetch('/devs-mentoring-api/courses/students-list.json')
+  .then(function(response) {
+    return response.text(); // tutaj otrzymujemy odpowiedź, z której wyciągamy JSON-a ...
+  })
+  .then(function(text) { // ... i przekazujemy tutaj
+    alert(text); // {"name": "Jan", "isStudent": true}
+  });
+
+  function myPromise(counter){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve(`MY PROMISE ${counter}`);
+        }, 2000);
+     })
+    }
+    
+    function callPromise(){
+      myPromise(1).then(result => console.log(result));    
+      myPromise(2).then(result => console.log(result));
+      myPromise(3).then(result => console.log(result));
+      console.log('LAST LOG');
+    }
+    callPromise();
+
+
+
+    async function job() {
+
+        let promise = new Promise((resolve, reject) => {
+          setTimeout(() => resolve("hello world"), 2000)
+        });
+      
+        let result = await promise; // czekaj, aż promise zwróci result
+        alert(result); // "Job done!"
+      }
+      
+      job();*/
+
+
+ console.log('Request data ...')   
+ 
+ /*setTimeout(() => {
+    console.log('Preparing data ...')
+
+const backendData = {
+    server: 'aws',
+    port: 2000,
+    status: 'working'
+    }
+
+    setTimeout(() => {
+        backendData.modified = true
+        console.log('Data received', backendData)
+    }, 2000)
+ }, 2000)*/
+      
+
+
+ const p = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        console.log('Preparing data ...')
+        const backendData = {
+            server: 'aws',
+            port: 2000,
+            status: 'working'
+        }
+        resolve(backendData)
+    }, 2000)
+ })
+
+ p.then((data) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.modified = true
+            resolve(data)
+        }, 2000)
+    })   
+}).then(clientData => {
+    console.log('Data received', clientData)
+})
